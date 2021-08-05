bc2dae2e0a18a4c7bf19dc5e3206b7b34c790884
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2021-08-05 01:41:27 -0300

    dinamic table

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { fruits } from './../../data/data';
 
 @Component({
   selector: 'app-table',
@@ -7,11 +8,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  @Input() data: any[];
+  display: string[] = ['Fruits','Total'];
+  rows: any[]= [];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(fruits);
+    this.createRows(fruits);
+  }
+
+  createRows(fruits: any[]) {
+    
+    for (let i = 0; i < fruits.length; i++) {
+      const fruit = fruits[i];
+      let total: number = 0;
+
+      this.rows.push({ data: 0, label: '' })
+      this.rows[i].label = fruit.label;
+
+      for (let y = 0; y < fruit.data.length; y++) {
+        const data = fruit.data[y].value;
+        total = parseInt(data) + total;
+      }
+
+      this.rows[i].data = total;
+    }
   }
 
 }
